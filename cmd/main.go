1790283716b51856fedfa5fcd56921cc98c0911e
Thomas Lo <thomasjinlo@gmail.com>
2024-06-02 00:47:23 -0700

    simplify ws server

--- cmd/main.go
+++ cmd/main.go
@@ -1,112 +1,72 @@
 package main
 
 import (
-	"encoding/json"
+	"bufio"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
-	"strings"
+	"os"
 
+	"github.com/go-chi/chi/v5"
 	"github.com/gorilla/websocket"
-	"github.com/nats-io/nats.go"
 )
 
-type User struct {
-	displayName string
-	conn *websocket.Conn
+type Connection struct {
+	clientId string
+	conn     *websocket.Conn
 }
 
-type ConnectBody struct {
-	ChannelIds []string
-	DisplayName string
-}
-
-type MessageBody struct {
-	ChannelId string
-	DisplayName string
-	Message string
-}
-
-type PublishMessage struct {
-	DisplayName string
-	Message string
-}
-
-func SetupRoutes() *http.ServeMux {
-	mux := http.NewServeMux()
-	channelToUsers := make(map[string][]*User)
+func setupRoutes() *chi.Mux {
+	connections := make(map[string]Connection)
 	upgrader := websocket.Upgrader{}
-	nc, err := nats.Connect(nats.DefaultURL)
+
+	r := chi.NewRouter()
+	r.Get("/connect", setupConnection(upgrader, connections))
+
+	return r
+}
+
+func setupConnection(u websocket.Upgrader, conns map[string]Connection) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("[gochatter ws] Setting up connection for client %v", r.RemoteAddr)
+		c, err := u.Upgrade(w, r, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer c.Close()
+		conn := Connection{
+			clientId: c.RemoteAddr().String(),
+			conn:     c,
+		}
+		conns[conn.clientId] = conn
+	}
+}
+
+func main() {
+	log.Print("[gochatter ws] Starting up GoChatter Websocket Server on port 8443")
+	mux := setupRoutes()
+	switch os.Args[1] {
+	case "client":
+		createClient()
+	default:
+		log.Fatal(http.ListenAndServe(":8443", mux))
+	}
+}
+
+func createClient() {
+	c, _, err := websocket.DefaultDialer.Dial("ws://localhost:8443/connect", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	mux.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
-		conn, err := upgrader.Upgrade(w, r, nil)
-		if err != nil {
-			log.Fatal(err)
-		}
+	defer c.Close()
 
-		displayName := r.Header.Get("DisplayName")
-		channelIds := r.Header.Get("ChannelIds")
+	scanner := bufio.NewScanner(os.Stdin)
 
-		user := &User{
-			displayName: displayName,
-			conn: conn,
-		}
-
-		for _, channelId := range strings.Split(channelIds, ",") {
-			log.Println(fmt.Sprintf("[CONNECT]: Subscribing to channel %s", channelId))
-			channelToUsers[channelId] = append(channelToUsers[channelId], user)
-		}
-	})
-
-	mux.HandleFunc("/message", func(w http.ResponseWriter, r *http.Request) {
-		body, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		var messageBody MessageBody
-		if err := json.Unmarshal(body, &messageBody); err != nil {
-			log.Fatal(err)
-		}
-
-		pubMsg := PublishMessage{
-			DisplayName: messageBody.DisplayName,
-			Message: messageBody.Message,
-		}
-		data, err := json.Marshal(pubMsg)
-		if err != nil {
-			log.Fatal(err)
-		}
-		nc.Publish(fmt.Sprintf("channel.%s", messageBody.ChannelId), data)
-	})
-
-	go func() {
-		nc.Subscribe("channel.*", func(m *nats.Msg) {
-			subjects := strings.Split(m.Subject, ".")
-			channelId := subjects[1]
-			log.Println(fmt.Sprintf("[SUBSCRIBE]: received message from channel %s", channelId))
-			log.Println(fmt.Sprintf("[SUBSCRIBE]: received data %v", string(m.Data)))
-			for _, user := range channelToUsers[channelId] {
-				var pubMsg PublishMessage
-				if err := json.Unmarshal(m.Data, &pubMsg); err != nil {
-					log.Fatal(err)
-				}
-				user.conn.WriteJSON(pubMsg)
-			}
-		})
-	}()
-
-	return mux
-}
-
-func main() {
-	mux := SetupRoutes()
-	err := http.ListenAndServe(":8443", mux)
-	if err != nil {
-		log.Fatal(err)
+	for {
+		fmt.Println("Enter message to WS: ")
+		scanner.Scan()
+		input := scanner.Text()
+		c.WriteMessage(websocket.TextMessage, []byte(input))
 	}
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,8 @@ module gochatter-ws
 go 1.21.3
 
 require (
-	github.com/gorilla/websocket v1.5.1 // indirect
-	github.com/klauspost/compress v1.17.0 // indirect
-	github.com/nats-io/nats.go v1.31.0 // indirect
-	github.com/nats-io/nkeys v0.4.5 // indirect
-	github.com/nats-io/nuid v1.0.1 // indirect
-	golang.org/x/crypto v0.14.0 // indirect
-	golang.org/x/net v0.17.0 // indirect
-	golang.org/x/sys v0.13.0 // indirect
+	github.com/go-chi/chi/v5 v5.0.12
+	github.com/gorilla/websocket v1.5.1
 )
+
+require golang.org/x/net v0.17.0 // indirect
